ec6ec7a11221a1226eef0bfe4c7d4fd7c45173b8
Juan Guerra <juan.guerra@lc.cuny.edu>
2017-03-15 10:47:25 -0400

    modified some stuff, nothing important

--- main.py
+++ main.py
@@ -9,44 +9,45 @@ def main():
 	quasarName="PG1115+080"
 	mapSize=500
 	BHmass=1.0e9*solarMass
+	print(BHmass)
 	spin='no spin'
 	R_E= 3.62e14
 	isco=quasar.isco(BHmass,"no spin")
-	# print(isco)
+	print(isco)
 	lRpixelSize=R_E*25./10000.
  	hRpixelSize=R_E*1./10000.
  	print(lRpixelSize/(3.0e8)) 
- 	# print(lRpixelSize)
+ 	print(lRpixelSize)
  	pixelSize= lRpixelSize #now given by whatever website
- 	# print(isco/pixelSize)
+ 	print(isco/pixelSize)
  	
-	# mapsSize=calculations.figureOutMapSize(BHmass,spin,pixelSize,isco)
-	# print(mapSize)
+	# # mapsSize=calculations.figureOutMapSize(BHmass,spin,pixelSize,isco)
+	# # print(mapSize)
 
-	#==================================================
+	# #==================================================
 
-	dMap=structures.distanceMap(mapSize,lRpixelSize)
-
-
-	dMap=structures.addGap(dMap,[0,6*isco],'mapValues')
-	# plotMethods.surfacePlot(dMap)
-	tMap=calculations.SStemperatureMap(dMap,isco,BHmass)
-	# plotMethods.surfacePlot(tMap)
-	intMap=calculations.IntensityMap(tMap,wBlue)
-	plotMethods.surfacePlot(intMap)
-	radius=dMap[10,450]
-	print(radius)
+	# dMap=structures.distanceMap(mapSize,lRpixelSize)
 
 
+	# dMap=structures.addGap(dMap,[0,6*isco],'mapValues')
+	# # plotMethods.surfacePlot(dMap)
+	# tMap=calculations.SStemperatureMap(dMap,isco,BHmass)
+	# # plotMethods.surfacePlot(tMap)
+	# intMap=calculations.IntensityMap(tMap,wBlue)
+	# plotMethods.surfacePlot(intMap)
+	# radius=dMap[10,450]
+	# print(radius)
 
 
 
 
-	# normalizedMap=calculations.normalize(intMap)
-	# print(normalizedMap[0,0:10])
-	# print(normalizedMap[0:10,0])
-	# plotMethods.surfacePlot(normalizedMap)
-	# plotMethods.writeToFile(normalizedMap)
-	# # plotMethods.surfacePlot(intMap)
+
+
+	# # normalizedMap=calculations.normalize(intMap)
+	# # print(normalizedMap[0,0:10])
+	# # print(normalizedMap[0:10,0])
+	# # plotMethods.surfacePlot(normalizedMap)
+	# # plotMethods.writeToFile(normalizedMap)
+	# # # plotMethods.surfacePlot(intMap)
 
 main()
